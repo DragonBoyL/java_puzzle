0b7f9ef6051155966ea5bd8622ec4a991e307877
lidonglong <1837068214@qq.com>
2024-03-04 11:55:09 +0800

    加载图片，并且打乱

--- src/ui/GameJframe.java
+++ src/ui/GameJframe.java
@@ -1,19 +1,70 @@
 package ui;
 
 import javax.swing.*;
+import java.util.Random;
 
 public class GameJframe extends JFrame {
+
+    //创建二维数组管理数据
+    int[][] data =new int[4][4];
     public GameJframe(){
 
         //初始化界面
         initJFrame();
 
-
         //初始化菜单
         initJMenuBar();
 
+        //初始化数据(打乱)
+        initData();
+
+        //初始化图片
+        initImage();
+
+
         //让代码显示
         this.setVisible(true);
+
+    }
+
+    private void initData() {
+        int[] tempArr={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        //打乱数组中数据的位置
+        Random r=new Random();
+        for (int i = 0; i < tempArr.length; i++) {
+            int index = r.nextInt(tempArr.length);
+            int temp=tempArr[i];
+            tempArr[i]=tempArr[index];
+            tempArr[index]=temp;
+        }
+
+        //放到二位数组
+        for (int i = 0; i < tempArr.length; i++) {
+            data[i/4][i%4]=tempArr[i];
+        }
+
+
+    }
+
+    private void initImage() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                //加载图片的序号
+                int number=data[i][j];
+                //ImageIcon
+                //ImageIcon icon=new ImageIcon("E:\\IDEA\\pintu\\资料\\素材\\image\\animal\\animal1\\2.jpg");
+                //JLable管理容器
+                JLabel jLabel1=new JLabel(new ImageIcon("E:\\IDEA\\pintu\\资料\\素材\\image\\animal\\animal3\\"+number+".jpg"));
+                //指定图片位置
+                jLabel1.setBounds(105*j,105*i,105,105);
+                //把管理容器添加到界面
+//        this.add(jLabel);
+                this.getContentPane().add(jLabel1);
+
+                number++;
+            }
+        }
+
 
     }
 
@@ -57,5 +108,8 @@ public class GameJframe extends JFrame {
         this.setLocationRelativeTo(null);
         //设置关闭模式
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        //取消默认的居中
+        this.setLayout(null);
     }
 }
